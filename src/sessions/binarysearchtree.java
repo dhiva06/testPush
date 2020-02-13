baefdbf1cffb7258af07db8656bdaa7a749bc278
dvelusamy <dvelusamy@XMCHELAP099.corp.xome.com>
2020-02-13 16:07:46 +0530

    test

---
+++ src/sessions/binarysearchtree.java
@@ -0,0 +1,15 @@
+package sessions;
+
+public class binarysearchtree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int data;
+		int left,right;
+		
+		
+
+	}
+
+}
---
+++ src/sessions/tofindindex.java
@@ -0,0 +1,14 @@
+package sessions;
+
+public class tofindindex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {2,4,6,8,10};
+		int k=8;
+		int len=arr.length-1;
+		int i=0;
+		
+	}
+
+}
